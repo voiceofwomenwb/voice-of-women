aed604f5932413e9c40b37c722ce4cfa972f1a68
voiceofwomenwb <wbengal2026@gmail.com>
2026-07-18 10:45:42 +0530

    Add Supabase client configuration

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const supabaseUrl = "https://cuvgpmiayohkvdebyugl.supabase.co";
+
+const supabaseKey = "sb_publishable_ZxL3S-RMOjO2O5nBWPQ7tA_D1K34pBf";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
